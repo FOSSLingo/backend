e71a1a52a51d50dc6b78436352bfd28bf754c0d5
iddu01 <157660303+iddu0@users.noreply.github.com>
2026-07-01 21:32:25 -0400

    start work on backend, i hate backend, ughhhh

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-fdsffdsf
+from fastapi import FastAPI
+import requests
+
+requests.request(method='GET', url='https://github.com/FOSSLingo/resources/archive/refs/heads/main.zip' )
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+  return {"message": "Hello, world!"}
